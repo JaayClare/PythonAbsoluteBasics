6cd8c643870aaf3047ab90264083e98382c5675d
Jaayclare <clare.james@chester.ac.uk>
2021-02-16 15:32:00 +0000

    Update Booleans.py

--- Booleans.py
+++ Booleans.py
@@ -2,7 +2,7 @@
 
 # A Boolean (or bool) can have two possible values, True or False
 
-# We see Booleans in if statements, while loops and more!
+# We see Booleans in if statements, while loops and more.
 
 # for example, lets look at the following if statement..
 # The if statement on line 12 needs to be 'True' for the print statement below to run.
@@ -14,10 +14,7 @@ if x > 5:
   print(x, 'is greater than 5!')
 else:
   print(x, 'is not greater than 5')
-  
+
 # We can also check this by running the following code
 
 print(x > 5) # This will print 'True' because x is greater than 5.
-        
-  
-        
