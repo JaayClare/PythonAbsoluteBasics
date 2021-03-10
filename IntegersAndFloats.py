736b4234632fdc4f6314a1561489bffb60b9b66d
Jaayclare <clare.james@chester.ac.uk>
2021-03-10 13:06:13 +0000

    Update IntegersAndFloats.py

--- IntegersAndFloats.py
+++ IntegersAndFloats.py
@@ -1,1 +1,30 @@
+### I have left all the print statements uncommented!
+### You may wish to go through and comment out parts that you aren't currently looking at
+### This guide is by no means super detailed! It's aimed purely at beginners to show the absolute basics.
 
+### Enjoy :)
+
+
+### Integers and Floats are the two main types of numbers that you'll be using in Python
+
+
+#### Integers ####
+
+# Integers are any positive/negative whole number, including 0
+a = 100
+b = -150
+c = 0
+
+
+# As seen in video 2, integers can be manipulated using a range of operators
+# Check out the 'Operators.py' file for a run through of all the operations
+
+
+# The type function shows that the variable a is an int.
+# This function is useful to use when checking the type of a variable before doing an operation
+print(type(a))
+
+# Integers are 'unbounded', this means that there is no limit on their size, it all depends on how much memory your PC has!
+
+
+#### Floats ####
