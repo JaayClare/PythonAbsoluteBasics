490848bd7ad5105f4b72fa0f3d9f9fddc3ffc3f5
Jaayclare <clare.james@chester.ac.uk>
2021-03-10 12:46:38 +0000

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,8 +1,6 @@
 ### I have left all the print statements uncommented!
-### You may wish to go through and comment out parts
-### that you aren't currently looking at
-### This guide is by no means super detailed! It's aimed purely at beginners
-### to show you the absolute basics.
+### You may wish to go through and comment out parts that you aren't currently looking at
+### This guide is by no means super detailed! It's aimed purely at beginners to show the absolute basics.
 
 ### Enjoy :)
 
