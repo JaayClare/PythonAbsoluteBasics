32b472de152657e2bc34eb53c58eb790483d9492
Jaayclare <clare.james@chester.ac.uk>
2021-04-21 10:17:41 +0100

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,1 +1,36 @@
+### I have left all the print statements uncommented!
+### You may wish to go through and comment out parts that you aren't currently looking at
+### This guide is by no means super detailed! It's aimed purely at beginners to show the absolute basics.
 
+
+
+
+
+
+numbers = [10,20,30,40,50]
+
+
+### Adding Elements ###
+# append: Adds one element to the end of the list
+# insert:
+# extend:
+
+
+### Returning Information about the list ###
+# count:
+# index:
+
+
+### Removing Elements from the list ###
+# pop:
+# remove:
+# clear:
+
+
+### Rearranging the list ###
+# reverse:
+# sort:
+
+
+### Copying the list
+# copy:
