f4bc76b41e35cfff2abc912e48a5e2f7afbd42bf
Jaayclare <clare.james@chester.ac.uk>
2021-11-16 09:18:50 +0000

    Update ForLoops.py

--- ForLoops.py
+++ ForLoops.py
@@ -1,1 +1,3 @@
-
+### I have left all the print statements uncommented!
+### You may wish to go through and comment out parts that you aren't currently looking at
+### This guide is by no means super detailed! It's aimed purely at beginners to show the absolute basics.
